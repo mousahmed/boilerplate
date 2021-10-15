1b67447f975f211e222b577886bcf563f1d1d636
mousahmed <mous.ahmed@outlook.com>
2021-10-15 17:09:30 +0200

    testing a bug fix

--- app/app/settings.py
+++ app/app/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
